d1f0c24876e299215779ece5df0a033aa6e557b1
Tomas Coox <tomas@joox.se>
2026-03-27 11:05:52 +0100

    feat: Update Hero image source URL parameters and dimensions.

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -6,10 +6,10 @@ const Hero = () => {
   return (
     <section id="hero" className="bg-white w-full">
       <Image
-        src="https://res.cloudinary.com/dlcfxe19d/image/upload/f_auto,q_auto,w_1920/v1774603872/BJ_17_jhvfgl.jpg"
+        src="https://res.cloudinary.com/dlcfxe19d/image/upload/f_auto,q_auto:best,w_2560/v1774603872/BJ_17_jhvfgl.jpg"
         alt="BlackJack Band"
-        width={1920}
-        height={875}
+        width={2560}
+        height={1167}
         priority
         className="w-full h-auto"
       />
