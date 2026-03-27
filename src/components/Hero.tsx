ffa27302fa84d1ada73fdd4e23eb82f0cbd5a2ab
Tomas Coox <tomas@joox.se>
2026-03-27 10:47:55 +0100

    refactor: Update Hero image dimensions and remove its container div.

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,16 +5,14 @@ import Image from 'next/image';
 const Hero = () => {
   return (
     <section id="hero" className="bg-white w-full">
-      <div className="container px-0 md:px-4">
-        <Image
-          src="https://res.cloudinary.com/dlcfxe19d/image/upload/f_auto,q_auto,w_1100/v1774603872/BJ_17_jhvfgl.jpg"
-          alt="BlackJack Band"
-          width={1100}
-          height={500}
-          priority
-          className="w-full h-auto"
-        />
-      </div>
+      <Image
+        src="https://res.cloudinary.com/dlcfxe19d/image/upload/f_auto,q_auto,w_1920/v1774603872/BJ_17_jhvfgl.jpg"
+        alt="BlackJack Band"
+        width={1920}
+        height={875}
+        priority
+        className="w-full h-auto"
+      />
     </section>
   );
 };
